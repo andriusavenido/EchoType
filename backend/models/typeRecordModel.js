3d7a0f17737c679f3decec065f02eb40fccdc1f0
Andrius <andriusavenido@gmail.com>
2024-07-22 21:00:01 -0400

    adding ignore and file structure

---
+++ backend/models/typeRecordModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const typeRecordSchema = new Scheme({
+    username:{
+        type: String,
+        required: true
+    },
+    user_id:{
+        type: Number,
+        required: true
+    },
+    cpm:{
+        type: Number,
+        required: true
+    },
+    wpm:{
+        type: Number,
+        required: true
+    },
+    performance_string:{
+        type: String,
+        required: true
+    },
+    map:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        required: true
+    }
+});
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+
+//express
+const express = require('express');
+const app = express();
+
+
+//middleware
+app.use(express.json());
+
+//routes
+
+//db connection
